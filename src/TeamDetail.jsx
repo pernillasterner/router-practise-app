306a548116e74d65010db434bfed9fd583459448
psterner <psterner@hotmail.com>
2023-11-09 11:14:47 +0100

    Find the id using params and fetch personal details

--- src/TeamDetail.jsx
+++ src/TeamDetail.jsx
@@ -1,3 +1,30 @@
+import { data } from "./data.json";
+import { useParams } from "react-router-dom";
+
 export const TeamDetail = () => {
-  return <div>TeamDetail</div>;
+  const { id } = useParams();
+
+  const teamData = data.find((t) => t.id === Number(id));
+
+  return (
+    <div className="profile-container">
+      <div className="profile-img">
+        <h2>Team member</h2>
+      </div>
+      <div className="profile-details">
+        <h2>Personal info</h2>
+
+        <p>Name: {teamData.name}</p>
+        <p>Birthday: {teamData.birthday}</p>
+        <p>Home: {teamData.home}</p>
+        <p>
+          Hobbies:
+          {teamData.hobbies.map((hobby, id) => (
+            <span key={id}>{hobby}</span>
+          ))}
+        </p>
+        <p>Favourite Animal: {teamData.favouriteanimal}</p>
+      </div>
+    </div>
+  );
 };
