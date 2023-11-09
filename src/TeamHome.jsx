65722284c866e47f41a14f887a76eccec5496724
psterner <psterner@hotmail.com>
2023-11-09 10:47:43 +0100

    Create json with team data and map in teamhome

--- src/TeamHome.jsx
+++ src/TeamHome.jsx
@@ -1,3 +1,17 @@
+import { data } from "./data.json";
+
 export const TeamHome = () => {
-  return <div>TeamHome</div>;
+  return (
+    <main>
+      <h2>Team Info</h2>
+      <section>
+        {data.map((id, name) => (
+          <div key={name} className={`card card-${id}`}>
+            <p>{name}</p>
+            <button id={id}>More info</button>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 };
